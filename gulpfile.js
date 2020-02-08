ded3945fb471a23d752e4c36b3cb2255bc47798c
Juch <Juch@live.cn>
2020-02-08 11:18:23 +0800

    把js、css全部集中到dev文件夹和gulp中

--- gulpfile.js
+++ gulpfile.js
@@ -4,25 +4,76 @@ var gulp = require('gulp'),
     cleanCSS = require('gulp-clean-css'), // 压缩css文件
     rename = require('gulp-rename'); // 文件重命名
 
-gulp.task('scripts', function(){
+/* 
+ * gulp - js
+ */
+gulp.task('index.js', function () {
     gulp.src('dev/js/index.js')
         .pipe(uglify())
-        .pipe(rename({suffix: '.min'}))
+        .pipe(rename({ suffix: '.min' }))
         .pipe(gulp.dest('assets/js'))
 });
 
-gulp.task('sass', function(){
+gulp.task('prism.js', function () {
+    gulp.src('dev/js/prism.js')
+        .pipe(uglify())
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(gulp.dest('assets/js'))
+});
+
+gulp.task('social-share.js', function () {
+    gulp.src('dev/js/social-share.js')
+        .pipe(uglify())
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(gulp.dest('assets/js'))
+});
+
+/* 
+ * gulp - css
+ */
+gulp.task('app.css', function () {
     gulp.src('dev/sass/app.scss')
         .pipe(sass())
         .pipe(gulp.dest('dev/sass'))
         .pipe(cleanCSS())
-        .pipe(rename({suffix: '.min'}))
+        .pipe(rename({ suffix: '.min' }))
         .pipe(gulp.dest('assets/css'));
 });
 
-gulp.task('watch', function(){
-    gulp.watch('dev/sass/*.scss', ['sass']);
-    gulp.watch('dev/js/*.js', ['scripts']);
+gulp.task('share.css', function () {
+    gulp.src('dev/sass/share.scss')
+        .pipe(sass())
+        .pipe(cleanCSS())
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(gulp.dest('assets/css'));
 });
 
-gulp.task('default', ['scripts', 'sass', 'watch']);
+gulp.task('prism.css', function () {
+    gulp.src('dev/sass/prism.scss')
+        .pipe(sass())
+        .pipe(cleanCSS())
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(gulp.dest('assets/css'));
+});
+
+gulp.task('github-markdown.css', function () {
+    gulp.src('dev/sass/github-markdown.scss')
+        .pipe(sass())
+        .pipe(cleanCSS())
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(gulp.dest('assets/css'));
+});
+
+gulp.task('sass', ['app.css', 'share.css', 'prism.css', 'github-markdown.css',]);
+
+gulp.task('script', ['index.js', 'social-share.js', 'prism.js']);
+
+gulp.task('default', function () {
+    gulp.run('sass', 'script');
+    gulp.watch('dev/sass/*.scss', function () {
+        gulp.run('sass');
+    });
+    gulp.watch('dev/js/*.js', function () {
+        gulp.run('script');
+    });
+});
